88b699d04481759ee0d67ed9d08e36fdb5fece05
Chainstrument <minitutoriel@gmail.com>
2024-09-24 10:48:16 +0200

    add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
+
  
 
 export default function Home() {
   return ( <div>
       <h1 className="text-4xl">Hello, world!</h1>
-      
+      <Button>Hello</Button>
   </div>
   );
 }
