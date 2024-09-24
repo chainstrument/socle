9d814b53622548c6fd7d6a32b2773891204af7aa
Chainstrument <minitutoriel@gmail.com>
2024-09-24 10:45:32 +0200

    install

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/src/components/ui/button";
-
+ 
 
 export default function Home() {
   return ( <div>
       <h1 className="text-4xl">Hello, world!</h1>
-      <Button>Click me</Button>
+      
   </div>
   );
 }
